6a33ab9c158166637120ee89f41c38a9b7b4fdea
NadaLetikaRidaIzzati <113491775+NadaLetikaRidaIzzati@users.noreply.github.com>
2022-09-27 12:37:48 +0700

    Add files via upload
    jdk 18.0.2.1
    SpringBoot  2.7.4
    Packaging jar
    Java 17

---
+++ user.service/user.service/src/main/java/com/nada/user/service/service/UserService.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.nada.user.service.service;
+
+import com.nada.user.service.dto.User;
+import java.util.List;
+
+/**
+ *
+ * @author LABSIKOMP03
+ */
+public class UserService {
+
+
+
+    public User getUser(String id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public void addUser(User user) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public User updateUser(User user) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public User deleteUser(String id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public List<User> getAllUsers() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
